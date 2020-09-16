94492ee3c64eec81b565fe42e5dd741600962659
Simon Knott <info@simonknott.de>
2020-09-16 08:16:35 +0200

    Replace "Read the docs" with "try the demo"

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -39,12 +39,12 @@ const Home: BlitzPage = () => {
               </div>
               <div className="mt-3 sm:mt-0 sm:ml-3">
                 <a
-                  href="https://github.com/quirrel-dev/next-quirrel"
+                  href="https://demo.quirrel.dev"
                   target="_blank"
                   rel="noreferrer"
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
                 >
-                  Read the Docs
+                  Try the Demo
                 </a>
               </div>
             </div>
