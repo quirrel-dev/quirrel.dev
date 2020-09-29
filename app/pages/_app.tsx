0916ac8f740d7e336185b9d02d2f4dbefffe1650
Simon Knott <info@simonknott.de>
2020-09-29 09:01:51 +0200

    Add Heap configuration

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,25 +1,35 @@
-import { AppProps, ErrorComponent } from "blitz"
+import { AppProps, ErrorComponent, Head } from "blitz"
 import { ErrorBoundary, FallbackProps } from "react-error-boundary"
 import { queryCache } from "react-query"
 import LoginForm from "app/auth/components/LoginForm"
-import { Suspense } from "react"
+import React, { Suspense } from "react"
 
 import "app/styles/index.css"
+
+const heapScript = `
+window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+heap.load("1150441566");
+`
 
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
-    <ErrorBoundary
-      FallbackComponent={RootErrorFallback}
-      onReset={() => {
-        // This ensures the Blitz useQuery hooks will automatically refetch
-        // data any time you reset the error boundary
-        queryCache.resetErrorBoundaries()
-      }}
-    >
-      <Suspense fallback={null}>{getLayout(<Component {...pageProps} />)}</Suspense>
-    </ErrorBoundary>
+    <>
+      <Head>
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: heapScript }} />
+      </Head>
+      <ErrorBoundary
+        FallbackComponent={RootErrorFallback}
+        onReset={() => {
+          // This ensures the Blitz useQuery hooks will automatically refetch
+          // data any time you reset the error boundary
+          queryCache.resetErrorBoundaries()
+        }}
+      >
+        <Suspense fallback={null}>{getLayout(<Component {...pageProps} />)}</Suspense>
+      </ErrorBoundary>
+    </>
   )
 }
 
