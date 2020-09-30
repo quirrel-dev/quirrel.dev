790cd30041c9bc39f7cd0a59e99cebe699d7cefa
Simon Knott <info@simonknott.de>
2020-09-30 10:13:04 +0200

    Refactor account section

--- app/projects/pages/dashboard.tsx
+++ app/projects/pages/dashboard.tsx
@@ -9,9 +9,109 @@ import { Form, Field } from "react-final-form"
 import deleteAccount from "app/account/mutations/deleteAccount"
 import { CardList } from "app/components/CardList"
 
+function DeleteAccountButton() {
+  const [showDeleteAccountModal, setShowDeleteAccountModal] = useState(false)
+  return (
+    <>
+      <Modal show={showDeleteAccountModal}>
+        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <div className="sm:flex sm:items-start">
+            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+              <svg
+                className="h-6 w-6 text-red-600"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                />
+              </svg>
+            </div>
+            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+              <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                Delete account
+              </h3>
+              <div className="mt-2">
+                <p className="text-sm leading-5 text-gray-500">
+                  Are you sure you want to delete your account? Your clients will lose Quirrel
+                  access immediately. This action cannot be undone.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+            <button
+              type="button"
+              className="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+              onClick={async () => {
+                await deleteAccount()
+                Router.push("/")
+              }}
+            >
+              Delete
+            </button>
+          </span>
+          <span className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+            <button
+              type="button"
+              className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+              onClick={() => setShowDeleteAccountModal(false)}
+            >
+              Cancel
+            </button>
+          </span>
+        </div>
+      </Modal>
+      <a
+        className="font-semibold text-red-500 hover:text-red-600 transition ease-in-out duration-150"
+        role="button"
+        tabIndex={-1}
+        onClick={() => setShowDeleteAccountModal(true)}
+        onKeyDown={() => setShowDeleteAccountModal(true)}
+      >
+        Delete Account
+      </a>
+    </>
+  )
+}
+
+function AccountSection() {
+  return (
+    <section id="account" className="mt-24">
+      <h1 className="text-4xl font-semibold text-gray-900 sm:text-xl sm:leading-7">Account</h1>
+
+      <ul className="space-y-2 mt-2">
+        <li>
+          <a
+            className="font-semibold text-teal-500 hover:text-teal-700 transition ease-in-out duration-150"
+            role="button"
+            tabIndex={-1}
+            onClick={() => {
+              window.alert("Not Implemented.")
+            }}
+            onKeyDown={() => {
+              window.alert("Not Implemented.")
+            }}
+          >
+            Upgrade to paid plan
+          </a>
+        </li>
+        <li>
+          <DeleteAccountButton />
+        </li>
+      </ul>
+    </section>
+  )
+}
+
 const Dashboard: BlitzPage = () => {
   const [showCreateProject, setShowCreateProject] = useState(false)
-  const [showDeleteAccountModal, setShowDeleteAccountModal] = useState(false)
   const [projectSlugs, projectSlugsMeta] = useQuery(getProjectSlugs, {})
 
   return (
@@ -152,93 +252,8 @@ const Dashboard: BlitzPage = () => {
           <span>Create Project</span>
         </button>
       </div>
-      <div className="mt-24">
-        <h1 className="text-4xl font-semibold text-gray-900 sm:text-xl sm:leading-7">Account</h1>
 
-        <ul className="space-y-2 mt-2">
-          <li>
-            <a
-              className="font-semibold text-teal-500 hover:text-teal-700 transition ease-in-out duration-150"
-              role="button"
-              tabIndex={-1}
-              onClick={() => {
-                window.alert("Not Implemented.")
-              }}
-              onKeyDown={() => {
-                window.alert("Not Implemented.")
-              }}
-            >
-              Upgrade to paid plan
-            </a>
-          </li>
-          <li>
-            <Modal show={showDeleteAccountModal}>
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <svg
-                      className="h-6 w-6 text-red-600"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                      />
-                    </svg>
-                  </div>
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-                      Delete account
-                    </h3>
-                    <div className="mt-2">
-                      <p className="text-sm leading-5 text-gray-500">
-                        Are you sure you want to delete your account? Your clients will lose Quirrel
-                        access immediately. This action cannot be undone.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-                    onClick={async () => {
-                      await deleteAccount()
-                      Router.push("/")
-                    }}
-                  >
-                    Delete
-                  </button>
-                </span>
-                <span className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-                    onClick={() => setShowDeleteAccountModal(false)}
-                  >
-                    Cancel
-                  </button>
-                </span>
-              </div>
-            </Modal>
-            <a
-              className="font-semibold text-red-500 hover:text-red-600 transition ease-in-out duration-150"
-              role="button"
-              tabIndex={-1}
-              onClick={() => setShowDeleteAccountModal(true)}
-              onKeyDown={() => setShowDeleteAccountModal(true)}
-            >
-              Delete Account
-            </a>
-          </li>
-        </ul>
-      </div>
+      <AccountSection />
     </div>
   )
 }
