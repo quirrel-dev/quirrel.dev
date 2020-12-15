f173abef482fc07ecfc5059c75aeb5221b17ae03
Simon Knott <info@simonknott.de>
2020-12-15 17:02:37 +0100

    Fix tests that depend on env vars

--- test/setup.ts
+++ test/setup.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect"
+import { config } from "dotenv"
+
+config({
+  path: ".env.local",
+})
