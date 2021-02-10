ecca0792719cc6cb0ea19ecdd00a9ea784e148ac
Simon Knott <info@simonknott.de>
2021-02-10 14:12:38 +0100

    Export query

--- app/incidents/queries/areThereIncidentsForProject.ts
+++ app/incidents/queries/areThereIncidentsForProject.ts
@@ -2,13 +2,17 @@ import { resolver } from "blitz"
 import db from "db"
 import * as z from "zod"
 
-resolver.pipe(resolver.zod(z.string()), resolver.authorize(), async (projectSlug, ctx) => {
-  const count = await db.incident.count({
-    where: {
-      tokenProjectOwnerId: ctx.session.userId,
-      tokenProjectSlug: projectSlug,
-    },
-  })
+export default resolver.pipe(
+  resolver.zod(z.string()),
+  resolver.authorize(),
+  async (projectSlug, ctx) => {
+    const count = await db.incident.count({
+      where: {
+        tokenProjectOwnerId: ctx.session.userId,
+        tokenProjectSlug: projectSlug,
+      },
+    })
 
-  return count !== 0
-})
+    return count !== 0
+  }
+)
