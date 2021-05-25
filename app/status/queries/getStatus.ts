fe218b7f9ec04f799fc569f3072d95548b94a857
Simon Knott <info@simonknott.de>
2021-05-25 13:28:25 +0200

    Don't error when status.quirrel.dev is down

--- app/status/queries/getStatus.ts
+++ app/status/queries/getStatus.ts
@@ -1,6 +1,11 @@
 import axios from "axios"
 
 export default async function getStatus() {
-  const { data } = await axios.get<string>("https://status.quirrel.dev")
-  return data.includes("All services are online") ? "up" : "down"
+  try {
+    const { data } = await axios.get<string>("https://status.quirrel.dev")
+    return data.includes("All services are online") ? "up" : "down"
+  } catch (error) {
+    console.error(error)
+    return "down"
+  }
 }
